17c6677096ae507ce2c41f14e97faae6033c6d68
Alumno <alumno@eduMacPolanco17.local>
2018-10-13 14:05:20 -0500

    Se implementa plugin de lightbox

---
+++ ejercicio2/js/main.js
@@ -0,0 +1,6 @@
+$(function(){ //funcion "Ready" ejecuta unicamente cuando ya se renderizo el codigo html java scrip es un archivo que interactua con html css
+
+    lightbox.option({
+  albumLabel: 'Imagen%1 de %2',
+      })
+});
